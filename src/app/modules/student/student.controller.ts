c13fcd616cad1495b2131495d1b33dc7727652ee
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-11-29 12:30:49 +0600

    added a studetn data successfully in mongodb

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -3,8 +3,8 @@ import { StudentServieces } from "./student.service";
 
 const createStudent = async(req: Request, res: Response) => {
     try {
-        const student = req.body;
-        const result = await StudentServieces.createStudentIntoDB(student);
+        const {student:studentData} = req.body;
+        const result = await StudentServieces.createStudentIntoDB(studentData);
         res.status(200).json({
             success: true,
             message: "Student Is Created Successfully",
