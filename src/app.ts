b2e53b7c76b3f4eb0efbdff5390566fb143bba8a
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-11-29 10:11:21 +0600

    complete project setup basic and fix framting issue with preetir install

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
-import cors from 'cors'
+import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
-const app:Application = express()
+const app: Application = express();
 
 // parser
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req:Request, res:Response) => {
-    const a = 20;
- 
-    res.send(a)
-})
+app.get('/', (req: Request, res: Response) => {
+  const a = 20;
+
+  res.send(a);
+});
 
 export default app;
