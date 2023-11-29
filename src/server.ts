5cf880329c425d0f4a99fbdb5d4becf69c83a637
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-11-29 09:15:27 +0600

    added installation monose express cors ts and config dotenv

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import config from "./app/config";
+
+const mongoose = require('mongoose');
+
+
+async function main() {
+    await mongoose.connect(config.database_url);
+
+
+  }
+  
+
+
+app.listen(config.port, () => {
+    console.log(`Example app listening on port ${config.port}`)
+  })
