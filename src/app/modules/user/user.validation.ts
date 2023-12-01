786feed7599ed71cf0997457ee61f40808393cfa
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-12-01 09:44:50 +0600

    Refactir user validation

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,13 +1,11 @@
 import { z } from "zod";
 
 const userValidationSchema = z.object({
-    id: z.string(),
-    password: z.string().max(20, { message: 'Password Can Not Be more than 20 carector' }),
-    needsPasswordChange: z.boolean().optional().default(true),
-    role: z.enum(['student', 'faculty', 'admin']),
-    status: z.enum(['in-progress', 'blocked']).default('in-progress'),
-    idDeleted: z.boolean().optional().default(false),
-    
+    password: z.string(
+        {
+            invalid_type_error:'Password must be string'
+        }
+    ).max(20, { message: 'Password Can Not Be more than 20 carector' }).optional(),    
 })
 
 export const userValidation = {
