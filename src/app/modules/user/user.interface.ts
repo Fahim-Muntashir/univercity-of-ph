4229622b4c064c73fbee8f619544bc1e745ec2a2
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-12-01 09:11:55 +0600

    user model add

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,5 +1,9 @@
 export type TUser = {
     id: string;
-    paddword: string;
+    password: string;
     needsPasswordChange: boolean;
+    role: 'admin' | 'student' | 'admin';
+    status: 'in-progress' | 'blocked';
+    isDeleted: boolean,
+
 }
